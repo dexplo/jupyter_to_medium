c2dd42ced6643c8c862cb81f1ee6d05c7a4b318c
tdpetrou <petrou.theodore@gmail.com>
2020-05-19 21:09:33 -0400

    refactor

--- jupyter_to_medium/__init__.py
+++ jupyter_to_medium/__init__.py
@@ -1,1 +1,2 @@
 from ._publish_to_medium import publish
+__version__ = '0.0.2'
--- jupyter_to_medium/_publish_to_medium.py
+++ jupyter_to_medium/_publish_to_medium.py
@@ -19,21 +19,19 @@ class Publish:
         self.filename = Path(filename)
         self.integration_token = self.get_integration_token(integration_token)
         self.pub_name = pub_name
+        self.dataframe_image = dataframe_image
         self.title = title or self.filename.stem
         self.tags = tags
         self.publish_status = publish_status
         self.notify_followers = str(notify_followers).lower()
         self.license = license
         self.canonical_url = canonical_url
-        self.nb_dir = self.filename.resolve().parent
-        self.images_dir = self.nb_dir / (self.filename.stem.replace(' ', '_') + '_files')
+        self.nb_home = self.filename.parent
         
         self.headers = self.get_headers()
         self.author_id = self.get_author_id()
         self.pub_id = self.get_pub_id()
-        if dataframe_image:
-            self.create_markdown()
-        self.md = self.get_markdown()
+        self.md, self.image_data_dict = self.create_markdown()
         self.load_images_to_medium()
         self.publish_to_medium()
 
@@ -41,7 +39,6 @@ class Publish:
         if not it:
             it_path = Path.home() / '.jupyter_to_medium' / 'integration_token'
             it = open(it_path).read().splitlines()[0]
-        print(it)
         return it
 
     def get_headers(self):
@@ -70,41 +67,68 @@ class Publish:
                          f'Here is the data returned {data}')
 
     def create_markdown(self):
-        from dataframe_image import convert
-        convert(self.filename, to='md')
+        if self.dataframe_image:
+            if self.dataframe_image is True:
+                self.dataframe_image = {'filename': self.filename, 'execute': False}
+            from dataframe_image._convert import Converter, convert
+            import inspect
+            arg_spec = inspect.getargspec(convert)
+            defaults = dict(zip(arg_spec.args[1:], arg_spec.defaults))
+            defaults.update(self.dataframe_image)
+            # must convert to markdown
+            defaults['to'] = 'md'
+            c = Converter(**defaults)
+            c.convert()
+            md = self.get_markdown(c)
+            image_data_dict = self.get_image_data(c)
+        else:
+            from nbconvert.exporters import MarkdownExporter
+            me = MarkdownExporter()
+            md, resources = me.from_filename(self.filename)
+            image_data_dict = resources['outputs']
+            image_data_dict = self.get_md_image_data(md, image_data_dict)
+        return md, image_data_dict
 
-    def get_markdown(self):
-        p = self.nb_dir / f'{self.filename.stem}.md'
-        return open(p).read()
+    def get_markdown(self, converter):
+        fn = converter.final_nb_home / (converter.document_name + '.md')
+        return open(fn).read()
 
-    def get_image_files(self):
+    def get_image_data(self, converter):
+        image_dir = converter.final_nb_home / converter.image_dir_name
+        image_data_dict = {}
+        for file in image_dir.iterdir():
+            data = open(file, 'rb').read()
+            rel_fn = Path(converter.image_dir_name) / file.name
+            image_data_dict[str(rel_fn)] = data
+        return image_data_dict
+
+    def get_md_image_data(self, md, image_data_dict):
         pat_inline = r'\!\[.*?\]\((.*?\.(?:gif|png|jpg|jpeg|tiff))'
         pat_ref = r'\[.*?\]:\s*(.*?\.(?:gif|png|jpg|jpeg|tiff))'
-        inline_files = re.findall(pat_inline, self.md)
-        ref_files = re.findall(pat_ref, self.md)
-        print(inline_files)
-        image_files = []
-        for file in inline_files + ref_files:
-            p = Path(file.strip())
-            if (self.nb_dir / p).exists() and p not in image_files:
-                image_files.append(p)
-        return image_files
+        inline_files = re.findall(pat_inline, md)
+        ref_files = re.findall(pat_ref, md)
+        all_files = [file.lower().strip() for file in inline_files + ref_files 
+                     if not file.lower().startswith('http')]
+        for file in all_files:
+            fn = self.nb_home / file
+            if fn.exists() and file not in image_data_dict:
+                image_data_dict[file] = open(fn, 'rb').read()
+        return image_data_dict
         
     def load_images_to_medium(self):
         all_json = []
-        image_files = self.get_image_files()
-        print(image_files)
-        for file in image_files:
-            extension = file.suffix[1:].lower()
+        for file, data in self.image_data_dict.items():
+            fp = Path(file)
+            extension = fp.suffix[1:].lower()
             if extension in self.IMAGE_TYPES:
                 print('loading image to medium')
-                name = file.stem
-                fileobj = (self.nb_dir / file).open(mode='rb')
-                file_payload = {'image': (name, fileobj, f'image/{extension}')}
+                name = fp.stem
+                file_payload = {'image': (name, data, f'image/{extension}')}
                 r = requests.post(self.IMAGE_URL, headers=self.headers, files=file_payload)
                 req_json = r.json()
                 new_url = req_json['data']['url']
-                self.md = self.md.replace(str(file), new_url)
+                # TODO: Find tool to make conversion to HTML links
+                self.md = self.md.replace(file.replace(' ', '%20'), new_url)
                 all_json.append(req_json)
         
         with open('medium_images_data_report.json', 'w') as f:
@@ -112,7 +136,7 @@ class Publish:
 
         # updated markdown
         new_md_filename = self.filename.stem + '_medium.md'
-        with open(self.nb_dir / new_md_filename, 'w') as f:
+        with open(self.nb_home / new_md_filename, 'w') as f:
             f.write(self.md)
 
     def publish_to_medium(self):
@@ -136,7 +160,7 @@ class Publish:
         self.result = requests.post(post_url, headers=self.headers, json=json_data)
         
 
-def publish(filename, integration_token=None, pub_name=None, dataframe_image=True, 
+def publish(filename, integration_token=None, pub_name=None, dataframe_image=False, 
             title=None, tags=None, publish_status='draft', notify_followers=False, 
             license='all-rights-reserved', canonical_url=None):
     '''
@@ -159,7 +183,33 @@ def publish(filename, integration_token=None, pub_name=None, dataframe_image=Tru
     pub_name : str, default `None`
         Name of Medium publication. Not necessary if publishing as a user.
     
-    dataframe_image : bool, default `True`
+    dataframe_image : bool or dict, default `False`
+        When False (default), the notebook will be converted to
+        markdown in its current state and sent to Medium.
+
+        When True, use the package dataframe_image to convert your 
+        notebooks to markdown. This is important if you have pandas
+        DataFrames as images, since Medium will just display the raw 
+        text otherwise.
+
+        Use a dictionary to supply keyword arguments to the `convert` 
+        function from dataframe_image. Below are the available keyword
+        arguments and their default values.
+
+        max_rows=30,
+        max_cols=10,
+        ss_width=1000,
+        ss_height=900,
+        resize=1,
+        chrome_path=None,
+        limit=None,
+        document_name=None,
+        execute=True,
+        save_notebook=False,
+        output_dir=None,
+        image_dir_name=None
+
+        
         Use package dataframe_image to convert the notebook to markdown.
         If you set this as `False`, then you must have your notebook 
         already converted to markdown.
