67bf817a00cc8bf1483a76bddccc74e845102550
tdpetrou <petrou.theodore@gmail.com>
2020-05-21 19:51:13 -0400

    format

--- setup.py
+++ setup.py
@@ -33,7 +33,5 @@ setuptools.setup(
     entry_points={'console_scripts': ['jupyter_to_medium=jupyter_to_medium._command_line:main']},
     include_package_data=True,
     data_files=[("etc/jupyter/nbconfig/notebook.d", [
-                    "jupyter-config/nbconfig/notebook.d/jupyter_to_medium.json"
-                    ])
-                    ]
-)
+                "jupyter-config/nbconfig/notebook.d/jupyter_to_medium.json"])]
+    )
