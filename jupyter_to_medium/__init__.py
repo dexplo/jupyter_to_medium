53cb77f78939e871870bc94892becaedb6eaafa5
tdpetrou <petrou.theodore@gmail.com>
2020-05-20 21:43:03 -0400

    bundler ext. paths

--- jupyter_to_medium/__init__.py
+++ jupyter_to_medium/__init__.py
@@ -1,2 +1,9 @@
 from ._publish_to_medium import publish
 __version__ = '0.0.2'
+
+def _jupyter_nbextension_paths():
+    return [dict(
+        section="notebook",
+        src="nbextension",
+        dest="jupyter_to_medium",
+        require="jupyter_to_medium/index")]
