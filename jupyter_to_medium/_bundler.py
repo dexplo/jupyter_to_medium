8092f438cb9c00475fd57a9ef76cbd3efd24f886
tdpetrou <petrou.theodore@gmail.com>
2020-05-22 20:55:55 -0400

    catch errors

--- jupyter_to_medium/_bundler.py
+++ jupyter_to_medium/_bundler.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import json
+import urllib.parse
 
 from tornado import gen
 
@@ -13,101 +15,65 @@ def _jupyter_bundlerextension_paths():
         "group" : "deploy",
     }]
 
-
-def get_html(xsrf_input, title):
-    html = f'''
-    <h1>Publish your Jupyter Notebook to Medium</h1>
-
-    <form method="GET">
-    {xsrf_input}
-
-    <h3>Integration token</h3>
-    <p>You may leave this field blank if your integration token is located in your home 
-       directory at '.jupyter_to_medium/integration_token'. If you don't have an integration 
-       token, <a href="https://github.com/Medium/medium-api-docs">learn how to request one 
-        from Medium.</a>
-    </p>
-    <input type="text" id="integration_token" name="integration_token"><br><br>
-    
-    <h3>Title</h3>
-    <input type="text" id="title" name="title" value="{title}"><br><br>
-
-    <h3>Publication Name</h3>
-    <input type="text" id="pub_name" name="pub_name"><br><br>
-
-    <h3>Tags</h3>
-    <p>Separate each tag with a comma. Max of 5</p>
-    <input type="text" id="tags" name="tags"><br><br>
-    
-    <h3>Publish Status</h3>
-    <div style="display: flex">
-        <label for="draft">Draft</label><br>
-        <input type="radio" id="draft" name="publish_status" value="draft" checked = "checked">
-        
-        <label for="public">Public</label><br>
-        <input type="radio" id="public" name="publish_status" value="public">
-
-        <label for="unlisted">Unlisted</label><br>
-        <input type="radio" id="unlisted" name="publish_status" value="unlisted">
-    </div>
-
-    <h3>Notify Followers</h3>
-    <div style="display: flex">
-        <label for="no">No</label><br>
-        <input type="radio" id="no" name="notify_followers" value="False" checked = "checked">
-        
-        <label for="yes">Yes</label><br>
-        <input type="radio" id="yes" name="notify_followers" value="True">
-    </div>
-
-    <h3>License</h3>
-    <select id="license" name="license">
-        <option value="all-rights-reserved">All Rights Reserved</option>
-        <option value="cc-40-by">cc-40-by</option>
-        <option value="cc-40-by-sa">cc-40-by-sa</option>
-        <option value="cc-40-by-nd">cc-40-by-nd</option>
-        <option value="cc-40-by-nc">cc-40-by-nc</option>
-        <option value="cc-40-by-nc-nd">cc-40-by-nc-nd</option>
-        <option value="cc-40-by-nc-sa">cc-40-by-nc-sa</option>
-        <option value="cc-40-zero">cc-40-zero</option>
-        <option value="public-domain">public-domain</option>
-    </select>
-    
-    <h3>Canonical URL</h3>
-    <input type="text" id="canonical_url" name="canonical_url"><br><br>
-
-    <input type="hidden" name="bundler" value="jupyter_to_medium_bundler">
-    <input type="submit" value="Publish">
-    </form>
-    '''
-    return html
-
 def upload(model, handler):
     arguments = ['title', 'integration_token', 'pub_name', 'tags', 
-                'publish_status', 'notify_followers', 'license', 'canonical_url']
+                'publish_status', 'notify_followers', 'license', 'canonical_url',
+                'chrome_path', 'download_markdown']
 
-    kwargs = {arg: handler.get_query_argument(arg) for arg in arguments}
+    kwargs = {arg: handler.get_query_argument(arg, None) for arg in arguments}
     path = model['path']
     kwargs['filename'] = path
-    kwargs['integration_token'] = kwargs['integration_token'] or None
-    kwargs['pub_name'] == kwargs['pub_name'] or None
-    kwargs['tags'] = kwargs['tags'].split(',')[:5]
-    kwargs['notify_followers'] = bool(kwargs['notify_followers'])
-    kwargs['canonical_url'] = kwargs['canonical_url'] or None
+    kwargs['integration_token'] = kwargs['integration_token'].strip() or None
+    kwargs['pub_name'] == kwargs['pub_name'].strip() or None
+    kwargs['tags'] = [tag.strip() for tag in kwargs['tags'].split(',')[:5]]
+    kwargs['notify_followers'] = kwargs['notify_followers'] == "True"
+    kwargs['canonical_url'] = kwargs['canonical_url'].strip() or None
+    kwargs['chrome_path'] = kwargs['chrome_path'].strip() or None
+    kwargs['download_markdown'] = kwargs['download_markdown'] == "True"
+   
+    try:
+        data = publish(**kwargs)
+    except ValueError as e:
+        message = getattr(e, 'message', repr(e))
+        data = {'app_status': 'fail', 
+                'error_data': message}
+    except Exception as e:
+        message = getattr(e, 'message', repr(e))
+        data = {'app_status': 'fail', 
+                'error_data': message}
+    else:
+        if 'data' in data:
+            data = data['data']
+            data['app_status'] = "success"
+        else:
+            data = {'app_status': 'fail', 
+                    'error_data': 'Error: \n' + str(data)}
 
-    json_data = publish(**kwargs)
-    return json_data
+    return data
 
-def success(handler, json_data):
-    url = json_data['data']['url']
-    title = json_data['data']['title']
-    html = f'''
-    <h1>Published to Medium!</h1>
-    <a href="{url}">Link to your Medium Post: {title}</a>
-    '''
-    handler.write(html)
+def read_html(name):
+    mod_path = Path(__file__).parent
+    html_path = mod_path / 'static' / f'{name}.html'
+    return open(html_path).read()
+
+def get_html_form(xsrf_input, title):
+    html = read_html('form')
+    return html.format(xsrf_input=xsrf_input, title=title)
+
+def get_html_success(data):
+    url = data['url']
+    title = data['title']
+    html = read_html('success')
+    return html.format(url=url, title=title)
+
+def get_html_fail(data):
+    error_data = data['error_data']
+    error_message = json.dumps(error_data)
+    html = read_html('fail')
+    return html.format(error_message=error_message)
+
     
-@gen.coroutine
+# @gen.coroutine
 def bundle(handler, model):
     """
     Parameters
@@ -117,17 +83,24 @@ def bundle(handler, model):
     model : dict
         Notebook model from the configured ContentManager
     """
-    title = handler.get_query_argument('title', None)
-    notebook_filename = Path(model['name']).stem
-    xsrf_input = handler.xsrf_form_html()
-    html = get_html(xsrf_input, notebook_filename)
-    if title is None:
+    app_status = handler.get_query_argument('app_status', None)
+    
+    if app_status is None:
+        xsrf_input = handler.xsrf_form_html()
+        notebook_filename = Path(model['name']).stem
+        html = get_html_form(xsrf_input, notebook_filename)
         handler.write(html)
-    else:
-        handler.write('Publishing to Medium...')
+    elif app_status == 'waiting':
+        html = read_html('waiting')
+        handler.write(html)
         handler.flush()
-        json_data = upload(model, handler)
-        success(handler, json_data)
-  
-    print(handler.request)
+        data = upload(model, handler)
+        if data['app_status'] == 'success':
+            html = get_html_success(data)
+        else:
+            html = get_html_fail(data)
+        
+        handler.write(html)
+
     handler.finish()
+  
