eec5eaf24a9c9de1c5f65dce5fadf71c75fddb33
tdpetrou <petrou.theodore@gmail.com>
2020-07-24 12:17:22 -0400

    reorg

--- setup.py
+++ setup.py
@@ -31,9 +31,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=['nbconvert', 'requests', 'matplotlib>=3.1', 'numpy', 'beautifulsoup4'],
     entry_points={'console_scripts': ['jupyter_to_medium=jupyter_to_medium._command_line:main']},
     include_package_data=True,
     data_files=[("etc/jupyter/nbconfig/notebook.d", [
                 "jupyter-config/nbconfig/notebook.d/jupyter_to_medium.json"])],
-    install_requires=['nbconvert', 'requests', 'matplotlib>=3.1', 'numpy', 'beautifulsoup4'],
     )
