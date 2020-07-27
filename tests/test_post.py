160ac8183b62a26195376501ddee711be7d565c8
tdpetrou <petrou.theodore@gmail.com>
2020-07-26 20:37:59 -0400

    one test

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,7 +1,8 @@
 import pytest
 import jupyter_to_medium as jtm
 
+
 class TestPost:
 
-    def test_short_post(self):
-        pass
+    def test_post(self):
+        jtm.publish('tests/notebooks/Test Medium Blog Post.ipynb')
