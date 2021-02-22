2b282c387db25dd640328457c47e5a5a301facd8
mjam03 <mjam03@users.noreply.github.com>
2021-02-22 17:54:10 +0000

    Update test_post.py with gistify test

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,12 +1,14 @@
 import pytest
 import jupyter_to_medium as jtm
 
+
+def test_gistify_post():
+    p = jtm.publish(
+        'tests/notebooks/Test Medium Blog Post.ipynb', gistify=True)
+    return p
+
+
 class TestPost:
 
     def test_post(self):
         jtm.publish('tests/notebooks/Test Medium Blog Post.ipynb')
-
-
-# def gistify_test_post():
-#    p = publish('tests/notebooks/Test Medium Blog Post.ipynb', gistify=True)
-#    return p
