41ce5b58b388919902967c176bb0a4b4358ebfd7
mjam03 <markjamison03@gmail.com>
2021-11-22 13:00:11 +0000

    Update tests to pull publish fronm file

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,18 +1,18 @@
-import jupyter_to_medium as jtm
+from ._publish_to_medium import publish
 
 
 class TestPost:
     def test_post(self):
-        jtm.publish("tests/notebooks/Test Medium Blog Post.ipynb")
+        publish("tests/notebooks/Test Medium Blog Post.ipynb")
 
     def test_empty_matplotlib(self):
-        jtm.publish("tests/notebooks/Test Empty Matplotlib Screenshot.ipynb")
+        publish("tests/notebooks/Test Empty Matplotlib Screenshot.ipynb")
 
     def test_image_creation(self):
-        jtm.publish("tests/notebooks/Test Image Creation.ipynb")
+        publish("tests/notebooks/Test Image Creation.ipynb")
 
     def test_gistify(self):
-        jtm.publish("tests/notebooks/Test Gistify Markdown.ipynb")
+        publish("tests/notebooks/Test Gistify Markdown.ipynb")
 
     def test_latex(self):
-        jtm.publish("tests/notebooks/Test Latex.ipynb")
+        publish("tests/notebooks/Test Latex.ipynb")
